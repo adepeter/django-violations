792f143b073080385ba21cd90efb497e1b3f4f33
Oluwaseun Peter <adepeter26@gmail.com>
2022-08-15 23:33:48 +0100

    Add default autofield type

--- violation/__init__.py
+++ violation/__init__.py
@@ -1,2 +1,2 @@
-VERSION = (1, 2, 0)
+VERSION = (1, 2, 1)
 __version__ = ".".join(str(i) for i in VERSION)
--- violation/apps.py
+++ violation/apps.py
@@ -3,6 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class ViolationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'violation'
     verbose_name = _('Violation')
 
